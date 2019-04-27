a0fcbd909224bcdb842cdc73e5199f0e9320fb18
bilel <bilelknh@gmail.com>
2019-04-27 16:08:43 +0200

    Mise en page et tableau des compétences ( pas encore fini à 100% )

--- stage2.php
+++ stage2.php
@@ -12,20 +12,48 @@ require("header.php");
 
                     <h2>Stage chez BBND</h2>
                     <img src="img/logobbnd.jpg"align="right">
-                    <p>Lors de ma deuxième j'ai effectué mon stage au sein de l'entreprise BBND ( Big Bang Network Developement )
-                        à Lille, BBND est une agence SSII dont l'activité s'oriente autour du développement informatique et de la communication multisupport.
-                        J'ai effectué 7 semaines au sein de leur structure lors de ce stage ( 07/01/2019 au 22/02/2019 ).
-                        Au cours de cette période j'ai eu l'occasion de travailler sur différents projets tel que une application lourde faite en java (Fidscan)
+
+                    <p>Lors de ma deuxième j'ai effectué mon stage au sein de l'entreprise BBND ( Big Bang Network Développement) à Lille, BBND est une agence SSII dont l'activité s'oriente autour du développement informatique et de la communication multisupport.
+                        J'ai effectué 7 semaines au sein de leur structure lors de ce stage ( 07/01/2019 au 22/02/2019).
+                        Au cours de cette période j'ai eu l'occasion de travailler sur différents projets tels qu'une application lourde faite en java (Fidscan)
                     </p>
+
                     <br>
+
                     <div class="row" >
                         <div class="col">
                             <img src="img/fid.png" height="500" width="700">
                         </div>
-                        <p>
-                            
-                        </p>
+
+                           <br> Fidscan est une application dites desktop c'est-à-dire qu'elle est utilisable uniquement sur ordinateur.
+                            <br>Le but de Fidscan est de pouvoir permettre aux entreprises de collecter les données de leurs clients via un QR code afin de leur proposer un programme de fidélité spécifique ou juste afin de pouvoir comptabiliser
+                            ses clients pour des futurs démarches.
+                           <br>Elle permet aussi aux particuliers d'éviter d'oublier leurs cartes de fidélités mais aussi de pouvoir créer rapidement sa carte juste en scannant le QR code de l'entreprise utilisant l'application.
+                           <br>Pour cela il existe aussi une application mobile qui est en cours de traitement chez BBND.
                     </div>
+
+                    <br>
+                    <div class="row">
+                        <div class="col">
+                            <img src="img/fid1.png" height="700" width="800">
+                        </div>
+                    <br>
+                        Pour commencer l'entreprise souhaitant utiliser Fidscan doit s'inscrire en renseignant diverses informations telles que son numéro de Siret, son code APE ou NAF, son adresse, etc.<br>
+                        En s'enregistrant sur l'application l'entreprise accédera à son panneau de controle avec diverses options telle que le choix du service quelle souhaite proposer ainsi que son QR code ou encore les données de ses clients.
+                    </div>
+                    <br>
+
+                    <div class="row">
+                        <div class="col">
+                            <img src="img/dbconnectfidmodif.png" height="500" width="500">
+                            <img src="img/mainfidmodif.png" height="500" width="500">
+                        </div>
+                        <br>
+                        Sur la première image, il s'agit du système de connexion à la base de données de l'application Fidscan avec l'adresse en local de celle-ci ainsi que les identifiants de connexion et le retour d'un message en cas d'erreur.
+                        <br>
+                        Sur la deuxième image il s'agit de la classe Main du programme c'est à dire celle qui lance la page de connexion de l'application Fidscan avec une requête permettant de récupérer toutes les données de la table entreprise avec les identifiants associé aux comptes.
+                    </div>
+
                 </div>
             </div>
         </div>
